7c50d1231972363b503dfb56f14a929c3332d6ef
odin2350 <odin2000@narod.ru>
2020-03-30 16:39:54 -0400

    update 2

---
+++ Chapter 2.py
@@ -0,0 +1,21 @@
+#In this chapter you’ll learn about the different
+#kinds of data you can work with in
+#your Python programs. You’ll also learn how
+#to store your data in variables and how to use
+#those variables in your programs.
+
+message = "Hello World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+#Strings
+
+"This is a string"
+'This is a string as well'
+
+'I told my friemd,"Python is my favorite language!"'
+"THe language 'Python' is named after Monty Python, not the snake."
+"One of Python's strenghts is it's diverse and supportive community"
+
