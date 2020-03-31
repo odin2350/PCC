45180b20663e24fe7c344d9da21425a9e574c642
odin2350 <odin2000@narod.ru>
2020-03-31 16:10:59 -0400

    3

--- Chapter 4.py
+++ Chapter 4.py
@@ -1,3 +1,16 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician.title())
+
+
+magicians = ['alice', 'lavid', 'carolina']
+for magician in magicians:
+    print((magician.title()) + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+magicians = ['alice', 'lavid', 'carolina']
+for magician in magicians:
+    print((magician.title()) + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+print("Thank you, everyone. That was a great magic show!")
