1cdd345368dfa11521af2564880be8ef1d97f505
odin2350 <odin2000@narod.ru>
2020-04-23 09:06:54 -0400

    update 9:06 am 04.23.2020

--- Chapter 7.py
+++ Chapter 7.py
@@ -43,19 +43,46 @@
 #else:
 #    print("your number is not multiple of 10")
 
-current_number = 1
-while current_number <= 5:
-    print(current_number)
-    current_number += 1
-print('\n')
-snumber = []
-for snumber in range(1,6):
-    print(snumber)
+#current_number = 1
+#while current_number <= 5:
+#    print(current_number)
+#    current_number += 1
+#print('\n')
+#snumber = []
+#for snumber in range(1,6):
+#    print(snumber)
 
 
-prompt = "\nTell me something, and I will repeat it back to you:"
-prompt += "\nEnter 'quit' to end the program. "
-message = ""
-while message != 'quit':
-    message = input(prompt)
-    print(message)
+#prompt = "\nTell me something, and I will repeat it back to you:"
+#prompt += "\nEnter 'quit' to end the program. "
+#message = ""
+#while message != 'quit':
+#    message = input(prompt)
+#    print(message)
+#prompt = "\nTell me something, and I will repeat it back to you:"
+#prompt += "\nEnter 'quit' to end the program. "
+#active = True
+#while active:
+#    message = input(prompt)
+#    if message == 'quit':
+#        active = False
+#    else:
+#        print(message)
+
+#prompt = "\nPlease enter the name of a city you have visited:"
+#prompt += "\n(Enter 'quit' when you are finished.) "
+#while True:
+#    city = input(prompt)
+#    if city == 'quit':
+#        break
+#    else:
+#        print("I'd love to go to " + city.title() + "!")
+
+#current_number = 0
+#while current_number < 10:
+#    current_number += 1
+#    if current_number % 2 == 0:
+#        continue
+#    print(current_number)
+
+#7-4. Pizza Toppings:
