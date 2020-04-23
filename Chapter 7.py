f220b131490c70eb798c0d32b0dc19305a582c39
odin2350 <odin2000@narod.ru>
2020-04-22 22:03:09 -0400

    new

---
+++ Chapter 7.py
@@ -0,0 +1,25 @@
+#message = input("Tell me something, and I will repeat it back to you: ")
+#print(message)
+#print('\n')
+#name = input("Please enter your name: ")
+#print("Hello, " + name + "!")
+
+#prompt = "If you tell us who you are, we can personalize the messages you see."
+#prompt += "\nWhat is your first name? "
+#prompt +=("\nteste")
+#prompt +=("\nteste")
+#prompt +=("\nso your name is ")
+#name = input(prompt)
+#print("\nHello, " + name + "!")
+
+#age = input("how old are you? ")
+#print(type(age))
+#age = int(age)
+#print(age >= 45)
+
+#height = input("How tall are you, in inches? ")
+#height = int(height)
+#if height >= 36:
+#    print("\nYou're tall enough to ride!")
+#else:
+#    print("\nYou'll be able to ride when you're a little older.")
