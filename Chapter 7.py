98df1f8016be90e8564e39075d120252d04caced
odin2350 <michaelsqa35@gmail.com>
2020-06-24 08:18:03 -0400

    06.24.220

--- Chapter 7.py
+++ Chapter 7.py
@@ -69,14 +69,14 @@
 #    else:
 #        print(message)
 
-#prompt = "\nPlease enter the name of a city you have visited:"
-#prompt += "\n(Enter 'quit' when you are finished.) "
-#while True:
-#    city = input(prompt)
-#    if city == 'quit':
-#        break
-#    else:
-#        print("I'd love to go to " + city.title() + "!")
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.) "
+while True:
+    city = input(prompt)
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to go to " + city.title() + "!")
 
 #current_number = 0
 #while current_number < 10:
